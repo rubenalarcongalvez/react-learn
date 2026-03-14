577b1aba3eb3f98f02f85218fca7404f04c4b945
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2026-03-14 16:22:15 +0100

    Tarea hecha

---
+++ app/1.crear-y-anidar-componentes/otro-componente.tsx
@@ -0,0 +1,7 @@
+// Aqui hago un componente como si fuera una función. Si queremos exportarlo, podemos usar export
+// De esta forma, podemos encapsular
+export function OtroComponente() {
+  return (
+    <div>Mira, estoy en otro archivo!</div>
+  )
+}
--- app/1.crear-y-anidar-componentes/page.tsx
+++ app/1.crear-y-anidar-componentes/page.tsx
@@ -1,7 +1,27 @@
+import { OtroComponente } from './otro-componente';
+
+// Podemos crear componentes tanto encapsulados como en el mismo archivo. Funcionan como si fueran funciones
+/* IMPORTANT: Los componentes de React siempre van la primera en mayúscula para diferenciarlos de los selectores HTML */
+function MiComponente() {
+  // IMPORTANT: El return debe ser con paréntesis (), no con {}
+  return (
+    <div>Esto ya sería un componente que podemos utilizar</div>
+  )
+}
+
+// Este componente es el componente principal del archivo, por lo que lo pondremos default
 export default function CrearYAnidarComponentesPage() {
   return (
+    // Aquí ya lo podremos usar, tanto en forma de componente asi
     <main>
-      Inicializado
+      {/* IMPORTANT: No se pueden instanciar los componentes 2 veces DENTRO del return. Deberemos poner un selector englobandolos */}
+
+      {/* <MiComponente></MiComponente> */}
+      {/* Como asi, y como estamos dentro de un selector que engloba ya todo, ya podemos ponerlo las veces que queramos */}
+      <MiComponente />
+      <MiComponente />
+      <p>* Lo duplico para que veamos dónde podemos instaurarlo y en qué contextos</p>
+      <OtroComponente/>
     </main>
   );
 }
