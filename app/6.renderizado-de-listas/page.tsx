8fc990ccbcb4d7023217b6267d8f75996e7f3997
rubenalarcongalvez <rubenalarcongalvez@gmail.com>
2026-03-21 20:48:10 +0100

    Renderizado de listas actualización

--- app/6.renderizado-de-listas/page.tsx
+++ app/6.renderizado-de-listas/page.tsx
@@ -1,10 +1,16 @@
 /* IMPORTANT: Es para poder mostrar y jugar con las listas en React */
 export default function RenderizadoDeListas() {
   const productos = [
-    { title: 'Banana', id: 1 },
-    { title: 'Caracoles', id: 2 },
-    { title: 'Patatas', id: 3 },
-    { title: '', id: 4 },
+    { id: 1, title: 'Banana', color: 'red' },
+    { id: 2, title: 'Tomate', color: 'red' },
+    { id: 3, title: 'Fresa', color: 'red' },
+    { id: 4, title: 'Arándanos', color: 'blue' },
+    { id: 5, title: 'Uvas', color: 'blue' },
+    { id: 6, title: 'Berenjena', color: 'blue' },
+    { id: 7, title: 'Ciruela', color: 'blue' },
+    { id: 8, title: 'Kiwi', color: 'green' },
+    { id: 9, title: 'Brócoli', color: 'green' },
+    { id: 10, title: '', color: 'green' },
   ];
 
   /* IMPORTANT: No podemos hacer un for para esto ya que React no lo renderiza bien en TSX */
@@ -16,14 +22,19 @@ export default function RenderizadoDeListas() {
 
   return(
     <>
-      <h1>Lista hecha en una variable</h1>
+      <h1>Lista hecha en una variable (sin ponerle la propiedad para el color)</h1>
       <ul>{listaProductos}</ul>
       <h1>Lista hecha en el propio HTML</h1>
       <ul>
         {/* Podemos tanto mapearlo aquí dentro como fuera en una variable */}
         {productos.map(producto => ( // IMPORTANT: También, en lugar de usar return, podemos usar () para englobar lo que queramos devolver
           /* Con key, definimos el tackBy (cuál es el id de la verdad distintivo) */
-          <li key={producto.id}>{producto?.title || 'Sin título'}</li>
+          <li 
+            key={producto.id}
+            style={{color: producto?.color || 'black'}}
+          >
+            {producto?.title || 'Sin título'}
+          </li>
         ))}
       </ul>
     </>
